c4b80630f0d49f221518d9e39f740937e5ad0bcd
Enzo Eitzen <enzoeitzen@gmail.com>
2024-09-02 00:52:36 +0100

    Add socials.

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
-import LainCarousel from "@/components/ui/lainCarousel";
+import {LainCarousel} from "@/components/ui/lainCarousel";
+import {SocialMediaLink} from "@/components/ui/SocialMediaLink";
 
 
 export default function Home() {
@@ -9,17 +10,16 @@ export default function Home() {
             <h1 className="m-5 font-black text-3xl flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-zinc-100 before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:via-zinc-300 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-zinc-700 before:dark:opacity-10 after:dark:from-zinc-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
                 Hello, I´m Enzo Ale</h1>
 
-            <p className="text-lg italic hover:not-italic">I am high school student, planning to study computer science. <br/> I mainly like
+            <p className="text-lg italic hover:not-italic text-center">I am a high school student, planning to study computer science. <br/> I mainly like
                 back end dev and IOT dev.</p>
-
         </div>
 
         <div
             className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-zinc-100 before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:via-zinc-300 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-zinc-700 before:dark:opacity-10 after:dark:from-zinc-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
             <p className={"font-extrabold italic text-black"}>Enzo </p>
-            <br/>
             <p className={"font-extrabold italic text-black"}><br/>Eitzen</p>
         </div>
+        <SocialMediaLink/>
 
     </main>);
 }
---
+++ src/components/ui/SocialMediaLink.jsx
@@ -0,0 +1,57 @@
+"use client";
+
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+import {Separator} from "@/components/ui/separator"
+import {Button, buttonVariants} from "@/components/ui/button";
+import {HoverCard, HoverCardContent, HoverCardTrigger} from "@/components/ui/hover-card";
+import Link from 'next/link';
+
+
+export function SocialMediaLink() {
+    return (
+        <>
+            <HoverCard>
+                <HoverCardTrigger asChild>
+                    <Button variant="link">@Socials</Button>
+                </HoverCardTrigger>
+                <HoverCardContent className="w-80">
+                    <div>
+                        <div className={"flex gap-3.5"}>
+                            <Avatar>
+                                <AvatarImage src="images/lain.jpg" alt="Lain Profile Picture"/>
+                                <AvatarFallback></AvatarFallback>
+                            </Avatar>
+                            <div className="space-y-1">
+                                <h4 className="text-sm font-medium leading-none">Hey!!</h4>
+                                <p className="text-sm text-muted-foreground">
+                                    Follow me on my socials, maybe we can become friends.
+                                </p>
+                            </div>
+                        </div>
+                        <Separator className="my-4"/>
+                        <div className="flex h-5 items-center space-x-4 text-sm">
+                            <Button asChild>
+                                <Link href="https://x.com/EitzenEnzo">Twitter</Link>
+                            </Button>
+                            <Separator orientation="vertical"/>
+                            <Button asChild>
+                                <Link href="https://github.com/EnzoEitzen">Github</Link>
+                            </Button>
+                            <Separator orientation="vertical"/>
+                            <Button asChild>
+                                <Link href="https://www.instagram.com/ale7cx/">Insta</Link>
+                            </Button>
+
+
+                        </div>
+                    </div>
+                </HoverCardContent>
+            </HoverCard>
+        </>
+
+    )
+}
